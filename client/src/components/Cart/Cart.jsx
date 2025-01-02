4a9a34cf2186ba1e9813119d9d4c9ce6d6c33218
Majid_89 <noble.majid1@gmail.com>
2025-01-02 16:42:34 +0500

    Background overlay added in cart model

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Cart = ({ cartToggle, setCartToggle }) => {
     return (
-        <>{
+        <>
+            {!cartToggle && <div className="bg-[rgba(0,0,0,0.5)] w-full h-screen fixed top-0 "></div>}
             <div className={`bg-black fixed ${cartToggle ? 'w-0' : 'w-96'} h-full top-0 right-0 text-white z-50 pt-24 duration-500`}>
                 <span className='cursor-pointer border-4 border-yellow-400' onClick={() => setCartToggle(!cartToggle)}>X Close</span>
             </div>
-        }
+
 
         </>
     )
