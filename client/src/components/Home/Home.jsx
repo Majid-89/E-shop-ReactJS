90ad246ce03cb2c228e0051350a7133309746f97
Majid_89 <noble.majid1@gmail.com>
2025-02-10 11:30:43 +0500

    Categories api called successfully

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,24 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Banner from './Banner/Banner'
 import Category from './Category/Category';
 import Products from '../Prouducts/Products'
+import { Context } from '../../utils/Context';
 import { fetchDataFromApi } from '../../utils/api';
 
 
+
 const Home = () => {
+    const { categories, setCategories } = useContext(Context)
 
     useEffect(() => {
         getCategories()
     }, [])
 
     const getCategories = () => {
-        fetchDataFromApi('/api/categories?populate=*').then((res) => console.log(res))
+        fetchDataFromApi('/api/categories?populate=*').then((res) => {
+            console.log(res)
+            setCategories(res)
+        })
     }
     return (
         <div>
             <Banner />
             <div className="max-w-[1200px] mx-auto w-[95%] xl:w-full">
-                <Category />
+                <Category categories={categories} />
                 <Products heading={"Popular Products"} />
             </div>
         </div>
